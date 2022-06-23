f1317a48fc38886889c94ef5f9383b1daac6dd8e
Nilgam <nilgam@proton.me>
2022-06-23 15:31:35 +0200

    First generation of the map done
    The script take dimension as argument, then create all tr>td to
    make the game map.
    
    Also, general script init the player position, then add a div.perso into
    right coordinates.

---
+++ js/map-generator.js
@@ -0,0 +1,32 @@
+class Map {
+    constructor(dimension) {
+        this.dimension = dimension;
+    }
+
+    create() {
+        let tbody = document.getElementsByTagName('tbody')[0];
+        Array.from(tbody.children).forEach((e) => e.remove());
+
+        for (let i = 0; i < this.dimension; i++) {
+            let tr = document.createElement('tr');
+            for (let j = 0; j < this.dimension; j++){
+                let td = document.createElement('td');
+                td.setAttribute('id', i+'-'+j)
+                tr.appendChild(td);
+            }            
+            tbody.appendChild(tr);
+        }
+    }
+
+    initPersonnage(){
+        let perso = new Personnage("Pipou", Math.floor(dimension / 2), dimension - 1);
+        let caseInit = document.getElementById(perso.y + "-" + perso.x)
+        console.log(caseInit);
+        let divPerso = document.createElement('div');
+        divPerso.setAttribute('id', 'perso');
+        caseInit.appendChild(divPerso);
+        return perso;
+    }
+    
+
+}
---
+++ js/script.js
@@ -0,0 +1,4 @@
+let dimension = 15;
+let map = new Map(dimension);
+map.create();
+let personnage = map.initPersonnage();
