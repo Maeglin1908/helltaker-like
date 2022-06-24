e61d37327e9e5572f9508dafa840e417879c8fa4
Maeglin1908 <benjamin.thiery1908@gmail.com>
2022-06-24 14:44:30 +0200

    maybe final commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 // let blocks = [];
 let dimension = 15;
-let map = new Map(dimension);
+let personnage; // = new Personnage();
+let map = new Map(1);
 map.create();
-let personnage = new Personnage();
 map.populateBackground();
-let wall = new Block(10, 4, "wall");
-let ladder = new Block(0, 9, "ladder");
-let box = new Block(5, 5, "box");
 
 // DONT ADD CODE AFTER THIS BLOCK
 // UNLESS ITS OTHERS EVENT LISTENERS
