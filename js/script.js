7420907a6049023aa7dd708603d5511387499429
Maeglin1908 <benjamin.thiery1908@gmail.com>
2022-06-24 12:14:17 +0200

    Implementation of boxes moves and win()
    Added ZQSD in addition of arrows
    
    Implmented list of blocks as static array

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
+// let blocks = [];
 let dimension = 15;
 let map = new Map(dimension);
 map.create();
-let personnage = new Personnage(map);
+let personnage = new Personnage();
 map.populateBackground();
 let wall = new Block(10, 4, "wall");
 let ladder = new Block(0, 9, "ladder");
 let box = new Block(5, 5, "box");
-
+// blocks.push(wall);
+// blocks.push(ladder);
+// blocks.push(box);
 // DONT ADD CODE AFTER THIS BLOCK
 // UNLESS ITS OTHERS EVENT LISTENERS
 
-window.addEventListener("keyup", event => {
+let handleKeyupEvent = window.addEventListener("keyup", event => {
     personnage.move(event.key);
+    map.isWin();
 });
