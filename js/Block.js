e77057829f1199bb0892c936b342edaabe304cb0
Nilgam <nilgam@proton.me>
2022-06-24 12:14:17 +0200

    Implementation of boxes moves and win()
    Added ZQSD in addition of arrows
    
    Implmented list of blocks as static array

--- js/Block.js
+++ js/Block.js
@@ -1,4 +1,6 @@
 class Block {
+    static blocks = [];
+
     constructor(y, x, type) {
         this.x = x;
         this.y = y;
@@ -9,5 +11,55 @@ class Block {
         divBlock.classList.add(this.type);
         divBlock.classList.add("block");
         caseInit.appendChild(divBlock);
+        Block.blocks.push(this);
+    }
+
+    static get(x, y) {
+        let rightBlock = null;
+        for (let b of Block.blocks) {
+            if (b.x == x && b.y == y) {
+                return b;
+            }
+        }
+        return null;
+    }
+
+    move(direction) {
+        let newX = this.x;
+        let newY = this.y;
+        switch (direction) {
+            case "ArrowUp":
+            case "z":
+                newY--;
+                break;
+            case "ArrowDown":
+            case "s":
+                newY++;
+                break;
+            case "ArrowLeft":
+            case "q":
+                newX--;
+                break;
+            case "ArrowRight":
+            case "d":
+                newX++;
+                break;
+            default:
+                break;
+        }
+        if (map.isTraversable(newX, newY)) {
+            this.updateBlockPosition(this.x, this.y, newX, newY);
+            this.x = newX;
+            this.y = newY;
+        } else {
+            console.log("Erreur déplacement");
+        }
+    }
+
+    updateBlockPosition(oldX, oldY, newX, newY) {
+        let caseBlock = document.querySelector('#case-' + oldY + '-' + oldX + ' .block');
+        let caseTarget = document.getElementById('case-' + newY + "-" + newX);
+        caseBlock.remove();
+        caseTarget.appendChild(caseBlock);
     }
 }
