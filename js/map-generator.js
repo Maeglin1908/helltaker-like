0535fa57b37f2cfa8d2f8feab955a89c9379ca19
Nilgam <nilgam@proton.me>
2022-06-24 09:37:15 +0200

    Update tiles ID
    tile id takes also "case-", cause can't start with a digit
    Ex : #case-1-10
    
    Also removed all tags into index.html, except div#game.
    As it, the scripts are portables

--- js/map-generator.js
+++ js/map-generator.js
@@ -7,23 +7,28 @@ class Map {
     }
 
     create() {
-        let tbody = document.getElementsByTagName('tbody')[0];
-        Array.from(tbody.children).forEach((e) => e.remove());
+        let terrain = document.getElementById("game");
+        let table = document.createElement('table');
+        table.setAttribute('id', 'map');
+
+        let tbody = document.createElement('tbody');
+        table.appendChild(tbody);
+        terrain.appendChild(table);
 
         for (let i = 0; i < this.dimension; i++) {
             let tr = document.createElement('tr');
-            for (let j = 0; j < this.dimension; j++){
+            for (let j = 0; j < this.dimension; j++) {
                 let td = document.createElement('td');
-                td.setAttribute('id', i+'-'+j)
+                td.setAttribute('id', 'case-' + i + '-' + j)
                 tr.appendChild(td);
-            }            
+            }
             tbody.appendChild(tr);
         }
     }
 
-    initPersonnage(){
+    initPersonnage() {
         let perso = new Personnage("Pipou", Math.floor(dimension / 2), dimension - 1, this);
-        let caseInit = document.getElementById(perso.y + "-" + perso.x)
+        let caseInit = document.getElementById('case-' + perso.y + "-" + perso.x)
         console.log(caseInit);
         let divPerso = document.createElement('div');
         divPerso.setAttribute('id', 'perso');
@@ -32,23 +37,27 @@ class Map {
         return perso;
     }
 
-    initEventListeners(){
+    initEventListeners() {
         window.addEventListener("keyup", event => {
             this.personnage.move(event.key);
         });
-           
     }
-    
-    
-    populateBackground(){
+
+    isTraversable(x, y) {
+        let innerBlock = document.querySelector('#case-' + y + '-' + x + ' .block');
+        // if(innerBlock )
+
+    }
+
+    populateBackground() {
         let backgroundGen = 0;
         Array.from(document.getElementsByTagName("td")).forEach((e) => {
-            let [x,y] = e.id.split("-");
-            let grassMin = Math.floor(this.width/3);
-            let grassMax = grassMin*2;
-            if(grassMin<=y && y<grassMax){
+            let [, x, y] = e.id.split("-");
+            let grassMin = Math.floor(this.width / 3);
+            let grassMax = grassMin * 2;
+            if (grassMin <= y && y < grassMax) {
                 backgroundGen = "pt-" + Math.floor(Math.random() * 7);
-            }else{
+            } else {
                 backgroundGen = "gr-" + Math.floor(Math.random() * 7);
             }
             e.classList.add(backgroundGen);
